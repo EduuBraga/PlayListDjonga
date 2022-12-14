69d42933510412cb4e3b41d88d2676664206ed11
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-14 11:04:29 -0300

    Refactor: Melhorando a semântica dos áudios

--- assets/JS/audio.js
+++ assets/JS/audio.js
@@ -1,48 +1,57 @@
 // ====== Arquivos dos áudios
-window.audios = [
-    {
-        music : "./assets/audios/1. Junho de 94.mp3",
-        thumb : "./assets/images/junhoDe94.jpg",
-        nome : "Junho De 94"
-    },
-    {
-        music : "./assets/audios/3. Leal.mp3",
-        thumb : "./assets/images/leal.jpg",
-        nome : "Leal"
-    },
-    {
-        music : "./assets/audios/4. Solto.mp3",
-        thumb : "./assets/images/solto.jpg",
-        nome : "Solto"
-    },
-    {
-        music : "./assets/audios/5. Ladrao.mp3",
-        thumb : "./assets/images/ladrão.jpg",
-        nome : "Ladrão"
-    },
-    {
-        music : "./assets/audios/6. Benca.mp3",
-        thumb : "./assets/images/Bença.jpg",
-        nome : "Bença"
-    },
-    {
-        music : "./assets/audios/7. Falcao.mp3",
-        thumb : "./assets/images/falcão.jpg",
-        nome : "Falcão"
-    },
-    {
-        music : "./assets/audios/2. O cara de oculos.mp3",
-        thumb : "./assets/images/oCaraDeOculos.jpg",
-        nome : "O Cara De Óculos"
-    },
-    {
-        music : "./assets/audios/8. Hat Trick.mp3",
-        thumb : "./assets/images/hatTrick.jpg",
-        nome : "Hat-Trick"
-    },
-    {
-        music : "./assets/audios/9. Olho de Tigre.mp3",
-        thumb : "./assets/images/olhoDeTigre.jpg",
-        nome : "Olho De Tigre"
-    }
+const audios = [
+  {
+    music: "./assets/audios/junho-de-94.mp3",
+    thumb: "./assets/images/junhoDe94.jpg",
+    nome: "Junho De 94",
+    id: 1
+  },
+  {
+    music: "./assets/audios/leal.mp3",
+    thumb: "./assets/images/leal.jpg",
+    nome: "Leal",
+    id: 2
+  },
+  {
+    music: "./assets/audios/solto.mp3",
+    thumb: "./assets/images/solto.jpg",
+    nome: "Solto",
+    id: 3
+  },
+  {
+    music: "./assets/audios/ladrao.mp3",
+    thumb: "./assets/images/ladrão.jpg",
+    nome: "Ladrão",
+    id: 4
+  },
+  {
+    music: "./assets/audios/benca.mp3",
+    thumb: "./assets/images/bença.jpg",
+    nome: "Bença",
+    id: 5
+  },
+  {
+    music: "./assets/audios/falcao.mp3",
+    thumb: "./assets/images/falcão.jpg",
+    nome: "Falcão",
+    id: 6
+  },
+  {
+    music: "./assets/audios/o-cara-de-oculos.mp3",
+    thumb: "./assets/images/oCaraDeOculos.jpg",
+    nome: "O Cara De Óculos",
+    id: 7
+  },
+  {
+    music: "./assets/audios/hat-trick.mp3",
+    thumb: "./assets/images/hatTrick.jpg",
+    nome: "Hat-Trick",
+    id: 8
+  },
+  {
+    music: "./assets/audios/olho-de-tigre.mp3",
+    thumb: "./assets/images/olhoDeTigre.jpg",
+    nome: "Olho De Tigre",
+    id: 9
+  }
 ]
