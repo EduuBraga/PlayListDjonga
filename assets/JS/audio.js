33240fb89dd36b24898a1267c23c1bc4347626d6
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-14 18:56:46 -0300

    Feat: Adicionei duração dos áudios

--- assets/JS/audio.js
+++ assets/JS/audio.js
@@ -4,54 +4,63 @@ const audios = [
     music: "./assets/audios/junho-de-94.mp3",
     thumb: "./assets/images/junhoDe94.jpg",
     nameMsc: "Junho De 94",
-    id: 1
+    id: 1,
+    duration: '05:29'
   },
   {
     music: "./assets/audios/leal.mp3",
     thumb: "./assets/images/leal.jpg",
     nameMsc: "Leal",
-    id: 2
+    id: 2,
+    duration: '03:42'
   },
   {
     music: "./assets/audios/solto.mp3",
     thumb: "./assets/images/solto.jpg",
     nameMsc: "Solto",
-    id: 3
+    id: 3,
+    duration: '03:42'
   },
   {
     music: "./assets/audios/ladrao.mp3",
     thumb: "./assets/images/ladrão.jpg",
     nameMsc: "Ladrão",
-    id: 4
+    id: 4,
+    duration: '04:32'
   },
   {
     music: "./assets/audios/benca.mp3",
     thumb: "./assets/images/bença.jpg",
     nameMsc: "Bença",
-    id: 5
+    id: 5,
+    duration: '03:54'
   },
   {
     music: "./assets/audios/falcao.mp3",
     thumb: "./assets/images/falcão.jpg",
     nameMsc: "Falcão",
-    id: 6
+    id: 6,
+    duration: '03:52'
   },
   {
     music: "./assets/audios/o-cara-de-oculos.mp3",
     thumb: "./assets/images/oCaraDeOculos.jpg",
     nameMsc: "O Cara De Óculos",
-    id: 7
+    id: 7,
+    duration: '04:17'
   },
   {
     music: "./assets/audios/hat-trick.mp3",
     thumb: "./assets/images/hatTrick.jpg",
     nameMsc: "Hat-Trick",
-    id: 8
+    id: 8,
+    duration: '04:29'
   },
   {
     music: "./assets/audios/olho-de-tigre.mp3",
     thumb: "./assets/images/olhoDeTigre.jpg",
     nameMsc: "Olho De Tigre",
-    id: 9
+    id: 9,
+    duration: '04:43'
   }
 ]
